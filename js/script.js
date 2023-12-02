39598b06a902ce1fd1ec59721520a1d31a500dff
faeze <faeze_zakipour@yahoo.com>
2023-12-02 08:56:18 -0800

    v1

---
+++ js/script.js
@@ -0,0 +1,65 @@
+let meno = document.getElementById("meno");
+let sub = document.getElementById("sub");
+let close1 = document.getElementById("close");
+meno.addEventListener("click", function () {
+  sub.style.display = "block";
+});
+close1.addEventListener("click", function () {
+  sub.style.display = "none";
+});
+
+
+
+//mahsoulat
+let thumbImage = document.querySelectorAll(".image2");
+let image = document.getElementById("image1");
+
+thumbImage.forEach((elem) => {
+  elem.addEventListener("click", function () {
+    image.src = `${elem.src}`;
+  });
+});
+
+//slidermahsoulat
+let plus = document.getElementById("plus");
+let count = document.getElementById("count");
+let minus = document.getElementById("minus");
+let counter = 0;
+plus.addEventListener("click", plusCounter);
+function plusCounter() {
+
+ if (counter < 8) {
+    counter++;
+    count.innerHTML = counter;
+  }
+  changePrice()
+}
+plusCounter();
+minus.addEventListener("click", minusCounter);
+function minusCounter() {
+  if (counter > 1) {
+    counter--;
+    count.innerHTML = counter;
+  }
+  changePrice()
+}
+function changePrice() {
+  let finalPrice = document.getElementById("totalPrice");
+  let price = `${counter * 12254000}`;
+  let finalP = [];
+  for (i in price) {
+    if (i == 0) {
+      finalP.push(price[price.length - 1]);
+    } else {
+      price = `${Math.floor((counter * 12254000) / 10 ** i)}`;
+      finalP.push(price[price.length - 1]);
+      if (i % 3 == 2) {
+        finalP.push(",");
+      }
+    }
+  }
+  if (finalP.length % 3 == 2) {
+    finalP.pop();
+  }
+  finalPrice.innerHTML = finalP.reverse().join("");
+}
